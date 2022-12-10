3ac482d2cb839bb3fa6c0a20cc29a214e79ac7bd
CaiClone <caiskove@gmail.com>
2022-12-10 20:55:04 +0100

    [UI] Add Icons to DeckUI

--- src/src/UI/UI_Card.gd
+++ src/src/UI/UI_Card.gd
@@ -5,11 +5,13 @@ var cardData
 var timer = 0
 
 onready var time_label: Label = $UI_Card/MarginContainer/HBoxContainer/TimeLabel
+onready var icon_rect: TextureRect = $UI_Card/MarginContainer/HBoxContainer/Control/Icon
 onready var ui_card: ColorRect = $UI_Card
 
 func set_card(card: Resource):
     cardData = card
     ui_card.color = cardData.col
+    icon_rect.texture = cardData.icon
     _set_active(false)
 
 func activate(duration: float):
@@ -27,6 +29,7 @@ func _process(delta):
 
 func _set_active(enabled: bool, anim_time: float = 0.75):
     time_label.set_visible(enabled)
+    icon_rect.set_visible(enabled)
     var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
     if enabled:
         tween.tween_property(ui_card, "margin_left", -1.0 * grow_size, anim_time)
