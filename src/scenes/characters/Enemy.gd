fb7aa66bf47f3df0de371383fc9649ba02d173f2
Joan <joancardonasa@gmail.com>
2022-12-10 11:38:01 +0000

    Create new map and remove print from enemy ready

--- src/scenes/characters/Enemy.gd
+++ src/scenes/characters/Enemy.gd
@@ -5,8 +5,6 @@ export var attack_speed_multiplier = 5
 
 onready var nav_agent = $NavigationAgent
 
-var path = []
-var path_node = 0
 
 var direction: Vector3 = Vector3.ZERO
 
@@ -15,7 +13,6 @@ var nav = null
 
 func _ready():
     player = Utils.get_player()
-    print(player)
 
 
 func _on_PathUpdateTimer_timeout():
@@ -27,7 +24,3 @@ func _physics_process(_delta):
     if is_instance_valid(player):
         move_and_slide(direction.normalized() * speed, Vector3.UP)
 
-#
-#func move_to(target_position: Vector3):
-#    path = nav.get_simple_path(global_transform.origin, target_position)
-#    path_node = 0
