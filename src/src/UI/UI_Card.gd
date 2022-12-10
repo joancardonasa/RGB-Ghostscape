f0c66224ba44f9163759851cb307cc0f80e988a2
CaiClone <caiskove@gmail.com>
2022-12-10 20:30:50 +0100

    [UI] Add animation to Deck UI

--- src/src/UI/UI_Card.gd
+++ src/src/UI/UI_Card.gd
@@ -7,12 +7,10 @@ var timer = 0
 onready var time_label: Label = $UI_Card/MarginContainer/HBoxContainer/TimeLabel
 onready var ui_card: ColorRect = $UI_Card
 
-func _ready():
-    _set_active(false)
-
 func set_card(card: Resource):
     cardData = card
     ui_card.color = cardData.col
+    _set_active(false)
 
 func activate(duration: float):
     timer = duration
@@ -31,10 +29,11 @@ func _set_active(enabled: bool, anim_time: float = 0.75):
     time_label.set_visible(enabled)
     var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
     if enabled:
-        tween.tween_property(ui_card, "margin_left", -1 * grow_size, anim_time)
+        tween.tween_property(ui_card, "margin_left", -1.0 * grow_size, anim_time)
+        ui_card.color = cardData.col
     else:
-        tween.tween_property(ui_card, "margin_left", 0, anim_time)
-    tween.parallel()
+        tween.tween_property(ui_card, "margin_left", 0.0, anim_time)
+        ui_card.color = cardData.col - Color(0, 0, 0, 0.2)
     
         
     
