7ed883038b247fdb7607a65301f4f3461a139d98
Joan <joancardonasa@gmail.com>
2022-12-10 13:25:22 +0000

    Implement enemy visibility depending on card

--- src/src/gameplay/cards/CardGreen.gd
+++ src/src/gameplay/cards/CardGreen.gd
@@ -4,14 +4,16 @@ class_name CardGreen
 export(float) var Duration = 5.0
 export(float) var SpeedMult = 2.0
 export(Color, RGB) var col = Color(0,1,0)
+export(bool) var reveal_enemies = false
+
 var _cardManager
 
 func set_card_manager(cardManager):
-	_cardManager = cardManager
+    _cardManager = cardManager
 
 func Enter():
-	_cardManager.emit_signal("Card_Color",col)
-	_cardManager.emit_signal("Player_SpeedMult",true, SpeedMult)
+    _cardManager.emit_signal("Card_Color",col)
+    _cardManager.emit_signal("Player_SpeedMult",true, SpeedMult)
 
 func Exit():
-	_cardManager.emit_signal("Player_SpeedMult",false, SpeedMult)
+    _cardManager.emit_signal("Player_SpeedMult",false, SpeedMult)
--- src/src/gameplay/cards/CardRed.gd
+++ src/src/gameplay/cards/CardRed.gd
@@ -3,15 +3,18 @@ class_name CardRed
 
 export(float) var Duration = 5.0
 export(Color, RGB) var col = Color(1, 0, 0)
+export(bool) var reveal_enemies = true
 
 var _cardManager
 
 func set_card_manager(cardManager):
-	_cardManager = cardManager
+    _cardManager = cardManager
 
 func Enter():
-	_cardManager.emit_signal("Player_AllowShoot",true)
-	_cardManager.emit_signal("Card_Color",col)
+    _cardManager.emit_signal("Player_AllowShoot", true)
+    _cardManager.emit_signal("Card_Color", col)
+    _cardManager.emit_signal("Enemy_Reveal", true)
 
 func Exit():
-	_cardManager.emit_signal("Player_AllowShoot",false)
+    _cardManager.emit_signal("Player_AllowShoot", false)
+    _cardManager.emit_signal("Enemy_Reveal", false)
