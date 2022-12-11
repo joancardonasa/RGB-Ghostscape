402690568453f0c31313aadb3dbce704a22127fd
CaiClone <caiskove@gmail.com>
2022-12-11 17:59:17 +0100

    [Platform] Add Bounce

--- src/src/gameplay/platform/PlatformManager.gd
+++ src/src/gameplay/platform/PlatformManager.gd
@@ -2,13 +2,14 @@ extends Node
 
 
 onready var _platforms: Array = get_children()
+export var perc_enable: float = 0.75
 
 func _ready():
     Utils.get_card_manager().connect("Platform_Reveal", self, "on_platform_reveal")
     for platform in _platforms:
-        platform.set_enabled(true)
+        platform.set_enabled(false)
 
 func on_platform_reveal(enabled: bool):
-    var affected = _platforms if not enabled else Utils.choice(_platforms, int(ceil(_platforms.size() * 0.3)))
+    var affected = _platforms if not enabled else Utils.choice(_platforms, int(ceil(_platforms.size() * perc_enable)))
     for platform in affected:
         platform.set_enabled(enabled)
