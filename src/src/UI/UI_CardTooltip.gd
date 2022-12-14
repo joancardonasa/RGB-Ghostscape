6603e775167bc0fd5736a4e985e04e23085a5e14
CaiClone <caiskove@gmail.com>
2022-12-14 23:29:23 +0100

    [UI] load tooltip with card_data and autoresize

--- src/src/UI/UI_CardTooltip.gd
+++ src/src/UI/UI_CardTooltip.gd
@@ -3,9 +3,15 @@ extends CanvasLayer
 onready var tooltip = $Tooltip
 var loaded: bool = false
 
+onready var _name = $Tooltip/MarginContainer/VBoxContainer/Name
+onready var _text = $Tooltip/MarginContainer/VBoxContainer/Text
+onready var _duration = $Tooltip/MarginContainer/VBoxContainer/Duration
+var base_size: float
+
 func _ready():
     #Hackiest way, we are in a jam
     yield(get_tree().create_timer(0.1), "timeout")
+    base_size = tooltip.rect_size.y
     _load()
 
 
@@ -23,8 +29,13 @@ func show_tooltip(card):
     _set_card(card)
     tooltip.show()
 
-func hide_tooltip(card):
+func hide_tooltip(_card):
     tooltip.hide()
 
 func _set_card(card):
-    pass
+    var data = card.card_data
+    _name.text = data.name
+    _text.text = data.tooltip
+    var textSize = _text.get_font("font").get_string_size(data.tooltip)
+    tooltip.rect_size.y = base_size + (textSize.y * _text.get_line_count())
+    _duration.text = "%.0fs" % data.Duration
