56159b33ed5268367da89b698911968517e41235
CaiClone <caiskove@gmail.com>
2022-12-15 01:10:47 +0100

    [UI] Clean up design, polish

--- src/src/UI/UI_CardTooltip.gd
+++ src/src/UI/UI_CardTooltip.gd
@@ -8,15 +8,18 @@ onready var _text = $Tooltip/MarginContainer/VBoxContainer/Text
 onready var _duration = $Tooltip/MarginContainer/VBoxContainer/Duration
 var base_size: float
 
+var _card_box
 func _ready():
     #Hackiest way, we are in a jam
     yield(get_tree().create_timer(0.1), "timeout")
     base_size = tooltip.rect_size.y
+    _card_box = Utils.get_ui_cardbox()
+    _card_box.connect("CardBox_Enabled", self, "hide_tooltip")
     _load()
 
 
 func _load():
-    for card in Utils.get_ui_cardbox().get_cards():
+    for card in _card_box.get_cards():
         card.connect("card_entered", self, "show_tooltip")
         card.connect("card_exited", self, "hide_tooltip")
     loaded = true
@@ -38,4 +41,4 @@ func _set_card(card):
     _text.text = data.tooltip
     var textSize = _text.get_font("font").get_string_size(data.tooltip)
     tooltip.rect_size.y = base_size + (textSize.y * _text.get_line_count())
-    _duration.text = "%.0fs" % data.Duration
+    _duration.text = "%.1fs" % data.Duration
