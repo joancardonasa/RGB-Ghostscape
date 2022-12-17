0ad061781b37083db1aa440a1c10fe1e0cabcdd8
Joan <joancardonasa@gmail.com>
2022-12-17 19:57:29 +0000

    Remake enemy look at logic

--- src/src/characters/EnemyGhost.gd
+++ src/src/characters/EnemyGhost.gd
@@ -4,7 +4,7 @@ func _move():
     if is_instance_valid(player) and _moving:
         move_and_slide(direction.normalized() * speed, Vector3.UP)
         var oriRot = global_rotation
-        look_at(nav_agent.get_next_location(), Vector3.UP)
+        look_at(player.global_transform.origin, Vector3.UP)
         global_rotation.x = oriRot.x
         global_rotation.z = oriRot.z
 
