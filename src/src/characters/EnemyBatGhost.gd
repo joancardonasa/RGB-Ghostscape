6de707d3c5a55532ac7c9f166d93e905de5d5452
Joan <joancardonasa@gmail.com>
2022-12-18 00:04:39 +0000

    Add new enemy logic

--- src/src/characters/EnemyBatGhost.gd
+++ src/src/characters/EnemyBatGhost.gd
@@ -7,14 +7,12 @@ var last_seen_player_pos: Vector3
 func _move(delta):
     if is_instance_valid(player) and _moving:
         #move_and_slide(direction.normalized() * speed, Vector3.UP)
-        var direction_to_player = global_transform.origin.direction_to(player.global_transform.origin)
-        global_transform.origin += direction_to_player.normalized() * (speed/2) * delta
         look_at(player.global_transform.origin, Vector3.UP)
+        var direction_to_player = last_seen_player_pos - global_transform.origin
+        global_transform.origin += direction_to_player.normalized() * (speed/2) * delta
 
 
 func _on_PlayerRaycastCheck_timeout():
-    var new_player_position: Vector3 = to_local(player.global_transform.origin)
-    player_raycast.cast_to = player_raycast.transform.origin.direction_to(new_player_position) 
-
-    if player_raycast.is_colliding():
+    player_raycast.cast_to = to_local(player.global_transform.origin)
+    if player_raycast.is_colliding() and player_raycast.get_collider() == player:
         last_seen_player_pos = player.global_transform.origin
